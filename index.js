60453965702f0577567f9e16d4fedc791e7f3387
Iuliia El Sammak <juliag875@gmail.com>
2022-01-09 13:54:26 -0500

    Done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 32;
+let num4 = 7;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 4, 6, 10, 20);
